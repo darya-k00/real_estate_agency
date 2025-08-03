bec9841b1fedb8004122f8e754e57f021bc44dce
darya-k00 <dasha.kon.060203@gmail.com>
2025-08-03 10:52:07 +0300

    Update admin.py

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint, Owner
 
 
-admin.site.register(Flat)
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ['owner']
+
+
+class FlatAdmin(admin.ModelAdmin):
+    inlines = [OwnerInline]
+    search_fields = ['town', 'address', 'owner']
+    readonly_fields = ['created_at']
+    list_display = ['__str__', 'new_building', 'construction_year']
+    list_editable = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    raw_id_fields = ['liked_by']
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ['apartments']
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ['flats']
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin)
+
